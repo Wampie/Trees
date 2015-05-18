b831e9a1c7cbcfc22aeb26b02a2461bae4aea6bc
Majander Aleksi J <ajmajand@dhcp-asv-104.helsinki.fi>
2015-05-18 15:26:42 +0300

    Node and Data, needs still way to compare values

--- src/treetester/TreeTester.java
+++ src/treetester/TreeTester.java
@@ -6,15 +6,52 @@
 
 package treetester;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author ajmajand
  */
 public class TreeTester {
 
-    /**
-     * @param args the command line arguments
-     */
+    private class TreeNode<T> {
+        private T data;
+        private TreeNode<T> parent;
+        private ArrayList<TreeNode<T>> children;
+        
+        public T getData() {
+            return data;
+        }
+        
+        public TreeNode<T> getParent() {
+            return parent;
+        }
+        
+        public void setParent(TreeNode<T> p) {
+            this.parent = p;
+        }
+        
+        public void addChildren(TreeNode<T> c) {
+            c.setParent(this);
+            children.add(c);
+        }
+        
+        public void addChildren(ArrayList<TreeNode<T>> list) {
+            for (TreeNode<T> treeNode : list) {
+                treeNode.setParent(this);
+            }
+            children = list;
+        }
+    }
+    
+    private class NodeData {
+        public String name;
+        public double ev;
+        public double entropy;
+        public int count;
+    }
+    
+    
     public static void main(String[] args) {
         // TODO code application logic here
     }
